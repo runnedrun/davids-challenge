3419a864a8cf76444343c342e5ad2761e1ba6a6f
David Gaynor <runnedrun@gmail.com>
2016-09-30 14:34:12 -0700

    Classadoo autocommit

---
+++ challenge3.js
@@ -0,0 +1,8 @@
+//// should log "SMALL NUMBER" 5 times, then BIG number 5 times
+//for (var i = 0, i < 10; i++) {
+//  if (i < 5) {
+//    console.log("BIG number")
+//  } else {
+//    console.log("small number")
+//  }	
+//}
